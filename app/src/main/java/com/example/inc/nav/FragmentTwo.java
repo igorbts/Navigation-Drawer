3e4e841c68e5fa7619ea41f102dacee322d1e1cb
igorbts <igorbts@hotmail.com>
2016-12-04 20:16:42 -0200

    Navigation Drawer
    apk android

---
+++ app/src/main/java/com/example/inc/nav/FragmentTwo.java
@@ -0,0 +1,19 @@
+package com.example.inc.nav;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by InC on 21/11/2016.
+ */
+
+public class FragmentTwo extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two,container,false);
+    }
+}
